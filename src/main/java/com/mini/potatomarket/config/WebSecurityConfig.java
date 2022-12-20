7013384d67bf902aacb9c182c06cf079fad13479
isladaisy <isladaisyf@gmail.com>
2022-12-20 22:38:54 +0900

    CHORE : cors 추석 처리 상태로 추가

--- src/main/java/com/mini/potatomarket/config/WebSecurityConfig.java
+++ src/main/java/com/mini/potatomarket/config/WebSecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -43,6 +46,27 @@ public class WebSecurityConfig {
                 .addFilterBefore(new JwtAuthFilter(jwtUtil),
                         UsernamePasswordAuthenticationFilter.class);
 
+//        http
+//                .cors();
+
         return http.build();
     }
+
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource(){
+//
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.addAllowedOrigin("http://localhost:3000");
+//        config.addAllowedOrigin("http://charleybucket.s3-website.ap-northeast-2.amazonaws.com"); //요거 변경하시면 됩니다.
+//        config.addExposedHeader(JwtUtil.AUTHORIZATION_HEADER);
+//        config.addAllowedMethod("*");
+//        config.addAllowedHeader("*");
+//        config.setAllowCredentials(true);
+//        config.validateAllowCredentials();
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return source;
+//    }
+
 }
